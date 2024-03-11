b9bed598e1e66a30b8df6215766974e5c9a89a4c
Ian Harland <harlandvibes@gmail.com>
2024-03-11 09:33:08 -0700

    Changes for mobile widths

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const kraken = document.getElementById('kraken');
 
 function toggleKraken() {
     if (kraken.innerHTML === 'Terminal Game - The Kraken') {
-        kraken.innerHTML = '<img src="./resources/images/krakenGame.png" style="max-width: 500px; height: auto;">'
+        kraken.innerHTML = '<img src="./resources/images/krakenGame.png" style="max-width: 300px; height: auto;">'
     } else {
         kraken.innerHTML = 'Terminal Game - The Kraken'
     }
@@ -18,7 +18,7 @@ const venue = document.getElementById('venue');
 
 function toggleVenue() {
     if (venue.innerHTML === 'San Diego Music Venue Finder') {
-        venue.innerHTML = '<img src="./resources/images/sdLiveMusic.png" style="max-width: 500px; height: auto;">'
+        venue.innerHTML = '<img src="./resources/images/sdLiveMusic.png" style="max-width: 300px; height: auto;">'
     } else {
         venue.innerHTML = 'San Diego Music Venue Finder'
     }
@@ -33,7 +33,7 @@ const cpu = document.getElementById('cpu');
 
 function toggleCPU() {
     if (cpu.innerHTML === 'CPU Simulator') {
-        cpu.innerHTML = '<img src="./resources/images/cpuSimulator.png" style="max-width: 500px; height: auto;">'
+        cpu.innerHTML = '<img src="./resources/images/cpuSimulator.png" style="max-width: 300px; height: auto;">'
     } else {
         cpu.innerHTML = 'CPU Simulator'
     }
@@ -48,7 +48,7 @@ const horoscope = document.getElementById('horoscope');
 
 function toggleHoroscope() {
     if (horoscope.innerHTML === 'Javascript Horoscope Generator') {
-        horoscope.innerHTML = '<img src="./resources/images/horoscope.png" style="max-width: 500px; height: auto;">'
+        horoscope.innerHTML = '<img src="./resources/images/horoscope.png" style="max-width: 300px; height: auto;">'
     } else {
         horoscope.innerHTML = 'Javascript Horoscope Generator'
     }
